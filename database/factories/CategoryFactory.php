74635d1507d5c668fdb5e6defa0980fec0addb88
juliozapatao96 <julio.cesar.zaor@gmail.com>
2023-12-12 06:38:36 -0500

    DB Category is added ->Category model is created with migration and factory and 5 categories are added with false data

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            // Es una clase de ayuda para el modo de desarrollo para crear datos falsos
+            // It is a helper class for develepment mode to create fake data
+            'name' => $this->faker->word;
+        ];
+    }
+}
