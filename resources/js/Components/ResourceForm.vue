bf9260503a08617d667019b18f9e26e7c23bd336
juliozapatao96 <julio.cesar.zaor@gmail.com>
2023-12-14 22:47:44 -0500

    Creation of a form to enter resources
    ->The ResourceForm component is created for the Form that receives the resources to be entered.
    
    ->It is tested to make sure that it is linked and obtaining the values of the form.

---
+++ resources/js/Components/ResourceForm.vue
@@ -0,0 +1,24 @@
+<script setup >
+import { ref } from "vue";
+let title = ref("");
+let description = ref("");
+let link = ref("");
+
+function createResource(){
+    console.log("Creando recurso");
+    console.log(title.value);
+    console.log(description.value);
+    console.log(link.value);
+}
+
+</script>
+
+<template>
+    <div class="m-8">
+        <!-- <p>{{ title }}</p> -->
+        <input type="text" v-model="title">
+        <input type="text" v-model="description">
+        <input type="text" v-model="link">
+        <button @click="createResource">Ingresar recurso</button>
+    </div>
+</template>
